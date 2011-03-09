65f71f2042e07903f5418572197221cf66fae548
Matt Campbell <woogley@gmail.com>
2011-03-08 23:03:42 -0500

    Creating a CoffeeScript template language

--- parser.js
+++ parser.js
@@ -1,18 +1,96 @@
-HTTPViewParser.parse = (function (parse) {
-	return function (view) {
-		var func, code;
-		try {
-			code = HTTPViewParser.convert(view, false);
-			func = eval(HTTPViewParser.addScope(CoffeeScript.compile(code)));
-		} catch (e) {
-			// This may not be CoffeeScript code.. fall back to JavaScript
-			if (!CoffeeScript.quiet) {
-				print('===\nCoffeeScript Error: ' + e.message + '\n\n' + code + '\n===');
-			}
-			func = parse(view);
+(function () {
+	var modes = {
+		OUTPUT: 0,
+		SCRIPT: 1
+	};
+
+	/* Converts view code into a JavaScript snippet to be eval'd */
+	function convert(input) {
+		var outputBuffer, instructions, mode, position, line;
+		outputBuffer = '';
+		instructions = [];
+		mode = modes.SCRIPT;
+		position = 0;
+		line = '';
+
+		function flushScript() {
+			instructions.push(outputBuffer);
+			outputBuffer = '';
 		}
 
-		print(func);
-		return func;
+		function flushOutput() {
+			/* Escape quotes */
+			var script = outputBuffer.replace(/\\/g, '\\\\').replace(/"/g, '\\"');
+
+			/* Remove leading newlines */
+			script = script.replace(/\r?\n/g, function (match, index) {
+				return index && '\\r\\n' || '';
+			});
+
+			/* Remove trailing whitespace */
+			script = script.replace(/\s*$/, '');
+
+			if (script.length) {
+				instructions.push(indent + 'output.print "' + script + '"');
+			}
+
+			outputBuffer = '';
+		}
+
+		while (position < input.length) {
+			if (input.charAt(position) === '\n') {
+				line = '';
+			}
+
+			if (mode === modes.SCRIPT) {
+				if (input.substr(position, 2) === '<%') {
+					indent = line.replace(/^(\s*).*$/, '$1'); 
+					flushScript();
+					mode = modes.OUTPUT;
+					position += 1;
+				} else {
+					outputBuffer += input.charAt(position);
+					line += input.charAt(position);
+				}
+			} else if (mode === modes.OUTPUT) {
+				if (input.substr(position, 2) === '%>') {
+					flushOutput();
+					mode = modes.SCRIPT;
+					position += 1;
+				} else {
+					outputBuffer += input.charAt(position);
+					line += input.charAt(position);
+				}
+			}
+
+			position += 1;
+		}
+
+		if (mode === modes.OUTPUT) {
+			flushOutput();
+		} else {
+			flushScript();
+		}
+
+		return instructions.join('');
 	};
-}(HTTPViewParser.parse));
+
+
+	HTTPViewParser.parse = (function (parse) {
+		return function (view) {
+			var func, code;
+			try {
+				code = convert(view);
+				func = eval(HTTPViewParser.addScope(CoffeeScript.compile(code)));
+			} catch (e) {
+				// This may not be CoffeeScript code.. fall back to JavaScript
+				if (!CoffeeScript.quiet) {
+					print('CoffeeScript Error: ' + e.message);
+				}
+				func = parse(view);
+			}
+
+			return func;
+		};
+	}(HTTPViewParser.parse));
+}());
